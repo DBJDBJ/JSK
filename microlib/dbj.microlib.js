ff2c34e877e0a88e15da2ba7cf6b6383cef35c4b
dbjdbj <dbjdbj@gmail.com>
2016-03-05 15:23:52 +0100

    microlib and util added
    reason of existence of certain functions here is still questionable

---
+++ microlib/dbj.microlib.js
@@ -0,0 +1,169 @@
+/*
+    DBJ*MICROLIB GPL (c) 2011 by DBJ.ORG
+*/
+// .net string.format like function
+// usage:   "{0} means 'zero'".format("nula") 
+// returns: "nula means 'zero'"
+// place holders must be in a range 0-99.
+// if no argument given for the placeholder, 
+// no replacement will be done, so
+// "oops {99}".format("!")
+// returns the input
+// same placeholders will be all replaced 
+// with the same argument :
+// "oops {0}{0}".format("!","?")
+// returns "oops !!"
+//
+if ("function" !== typeof String.format)
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/\{(\d|\d\d)\}/g, function ($0) {
+            var idx = 1 * $0.match(/\d+/)[0]; return args[idx] !== undefined ? args[idx] : (args[idx] === "" ? "" : $0);
+        }
+            );
+    }
+    /*
+    */
+var dbj = {
+	
+    dummy: (function () {
+        var TOS_ = Object.prototype.toString,
+        HOP_ = Object.prototype.hasOwnProperty;
+        // global helpers
+        if ("function" !== typeof window.roleof) {
+            window.roleof = function (o) { return TOS_.call(o).match(/\w+/g)[1]; }
+        }
+        if ("function" !== typeof window.isArray) {
+            window.isArray = function (x) { return roleof(x) === "Array"; }
+        }
+        if ("function" !== typeof window.isObject) {
+            window.isObject = function (x) { return roleof(x) === "Object"; }
+        }
+    } ()),
+
+
+    XMLHttpFactories: [
+	function () { return new XMLHttpRequest() },
+	function () { return new ActiveXObject("Msxml2.XMLHTTP") },
+	function () { return new ActiveXObject("Msxml3.XMLHTTP") },
+	function () { return new ActiveXObject("Microsoft.XMLHTTP") }
+     ],
+
+    createXMLHTTPObject: function () {
+        var i = dbj.XMLHttpFactories.length, e;
+        while (i--) {
+            try {
+                return dbj.XMLHttpFactories[i]();
+            }
+            catch (e) {
+                /**/
+            }
+        }
+        alert("dbj.createXMLHTTPObject() failed ?");
+        return false;
+    },
+    $: function (selector, el) {
+        return (el || document).querySelector(selector);
+    },
+    $$: function (selector, el) {
+        return (el || document).querySelectorAll(selector);
+    },
+    later: function (context, func, timeout) {
+        /* execute function a bit later, default timeout is 1 micro sec,execution context is global name space
+        arguments after 'timeout', are passed to the callback
+        */
+        var args = timeout ? [].slice.call(arguments, 3) : [].slice.call(arguments, 1);
+        context || (context = (this || window));
+        var tid = setTimeout(function () {
+            clearTimeout(tid); tid = null; delete tid;
+            func.apply(context, args);
+        }, timeout || 1);
+    },
+
+    /* 
+	multipuropse logging, aserting etc... 
+	always using the console object
+	but adding functionality
+    */
+	console = (function () {
+		    var
+			console_cmd = { 
+				"log" 		: console.log, 
+				"assert" 	: console.assert , 
+				"info" 		: console.info, 
+				"error" 	: console.err,
+				"begin"     : group_begin ,
+				"end"       : group_end
+			},
+			group_begin = function ()  { console.group("dbj MICROLIB"); },
+			group_end   = function ()  { console.groupEnd(); };
+			/*
+			 argument format
+			 { cmd : "log", obj : object, msg : "message", grp: true | false }
+			*/
+			return  function ( arg_ )  {
+				var cmd = arg_["cmd"], s_ = arg_["msg"], group_ = arg["grp"], obj_ = arg["obj"];
+					if ( cmd in console_cmd ) {
+						if ( group_ ) group_begin();
+							  dbj.console_cmd[cmd](obj_, s_ );
+						if ( group_ ) group_end();
+					} else {
+						group_begin();
+							console.err("dbj.console() received a wrong command?");
+						group_end();
+					}
+			} 
+	}()),
+		assert : function (x) {
+			if (!x) {
+				dbj.console({ cmd:"error", msg:"dbj.assert failed for: " + x, grp : true }) ; 
+				throw "dbj.assert failed for: " + x ;
+			}
+		},
+		print: function ( s_, grouped ) {
+			dbj.console({ cmd: "log", obj: s_, grp: grouped } ) ;
+		} ,
+		
+		print_grp_begin : function () {
+			dbj.console({ cmd: "begin" } ) ;
+		},
+		
+		print_grp_end   : function () {
+			dbj.console({ cmd: "end" } ) ;
+		},
+
+        evil : (function (x) {
+		// eval in presence of 'use strict'	
+            "use strict";
+		// spn = secret property name
+		    var spn = "evil_" + (+new Date());
+            return function (x) {
+				(1, eval)("top['" + spn + "'] = (" + x + ")");
+				return top[spn];
+			}
+        }()),
+		/*
+		    one of those mind bending things for JS begginers
+			usefull for assuring value is a number
+			of course if it is not a object when it is number NaN
+			num ("1.1")  --> number 1.1 
+			num ("1")    --> number 1
+			num (new Date()) --> number 1457181273435
+			num ( new String() ) --> number 0
+			num ( new Object() ) --> number NaN
+		*/
+	    num : function num (a) {
+			return a - 0 ;
+        } ,
+        /* quick number rounder */
+        round: function (original_number, decimals) {
+            var POW = Math.pow(10, decimals), 
+				V1 	= original_number * POW, 
+				V2 	= Math.round(V1);
+            return V2 / POW;
+        }
+} /* eof dbj {} */
+
+
+} ());
+
---
+++ microlib/dbj.utl.js
@@ -0,0 +1,145 @@
+
+/*
+ MIT,GPL (c) 2009-2010 by DBJ.ORG
+
+ a little javascript set for my development and testing pages
+
+ $Revision: 9 $$Date: 7/03/11 0:12 $
+----------------------------------------------------------------------------------------------
+if unlucky and debugging inside IE <= 8 include this script once on the top of your html page
+as it will provoke inclusion of firebug-lite
+
+    if (!window.console)
+        document.write(
+    '<!--[if lte IE 8]>' +
+    '<script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"><' + '/script>' +
+    '<![endif]-->'
+    );
+
+*/
+if (!this.dbj) { alert("This file requires previous inclusion of dbj root object which is in dbj.microlib.js"); }
+else {
+    "use strict";
+
+/* 
+help summarizing or averaging values saved in this cache 
+of key/value pairs where values are always numerical values 
+internal obj_ is object where each property is an array
+example:
+        dbj.summa.add("A",1).add("A",2).add("B",[100,1]) ;
+		dbj.summa.sum("A") returns 3
+		dbj.summa.sum("B") returns 101
+		dbj.summa.sum("C") returns 0
+*/
+  summa : (function () {
+            var obj_ = {},
+        sum_ = function (arr) { var l = arr.length, sum = 0; while (l--) { sum += arr[l]; }; return sum; },
+        avg_ = function (arr) { return sum_(arr) / arr.length; },
+        num_ = function (arr) { var l = arr.length, sum = 0; while (l--) { arr[l] = arr[l] - 0; }; return arr; } ;
+
+            return {
+                /* interface */
+                add: function (k, v) {
+
+                    if (! isArray(v) ) { v = [v - 0]; }
+                     else v = num_(v) ;
+
+                    if (!isArray(obj_[k]))
+                        obj_[k] = [].concat( v );
+                    else
+                        obj_[k] = obj_[k].concat( v );
+                    return this;
+                },
+                sum: function (k) { return sum_(obj_[k] || []); },
+                avg: function (k) { return avg_(obj_[k] || []); },
+                all: function (k) { return (obj_[k] ? [].concat(obj_[k])   : []) },
+                rst: function (k) { if (obj_[k]) { var old = obj_[k]; obj_[k] = []; return old; } return [];}
+            };
+		}()),
+        /*
+        use this function to harvest form values on inputs named in its "defaults" argument
+        example call :
+        var harvest = harvester("myForm", { "name" : "Default", "age" : 22, "sex" : "male" } );
+        look for inputs name, age and sex in the form "myForm". if input value is null use the
+        values given in the argument.
+        */
+        harvester: function (frm_id, defaults) {
+            var $frm = jQuery("#" + frm_id, document.object), $input,
+            getval = function (id_) {
+                $input = $frm.find("input#" + id_);
+                return ($input.val() || defaults[id_]);
+            };
+            for (name in defaults) { defaults[name] = getval(name); }
+            return defaults;
+        },
+
+        table: function (host, id, klass, undefined) {
+            /*
+            very simple but effective table 'writer'
+
+            all arguments are optional
+            var tabla = dbj.table(your_host_dom_element, "your_table_id", "your_css_class_name");
+
+            methods are chainable
+
+            tabla.hdr("ID", "Name", "Average Rating")  // defines table of 3 columns
+                 .caption("Waiting for " + query[1])
+
+                 .row(1,"Bob",3.5) // proceed with SAME number of columns
+                 .row(2,"DBJ",2.5); // 
+
+            optionaly style the table made, for example:
+            tabla.uid( function( id_ ) { $("#"+id_ ).dataTable()  }); // apply 'dataTable' jQuery plugin 
+
+            */
+            host || (host = document.body);
+            id || (id = "dbjtable_" + (0 + new Date));
+            klass || (klass = "dbjtable");
+            var
+            slice = Array.prototype.slice,
+            /* attach to table if exist */
+            $existing = $("#" + id),
+            table = $existing[0] ? $existing : jQuery("<table id='{0}' class='{1}'><caption></caption><thead></thead><tbody></tbody>".format(id, klass)).appendTo(host),
+            $table = jQuery(table[0], host), colcount = null;
+            delete table;
+
+            /* 
+            first row added defines number of columns 
+            latter can make row with different number; the table will be jaddged
+            */
+            function to_row(row_, header) {
+                if (jQuery.isArray(row_)) {
+                    if (!colcount) colcount = row_.length;
+                    var td = header ? "TH" : "TD", wid = Math.round(100 / colcount);
+                    td += " width='{0}%' ".format(wid);
+                    row_ = row_.join("</{0}><{0}>".format(td));
+                    return "<tr><{0}>{1}</{0}></tr>".format(td, row_);
+                }
+                else {
+                    throw "to_row() first argument must be array";
+                }
+            }
+            return {
+                hdr: function () {
+                    $table.find("thead").append(to_row(slice.call(arguments), true)); return this;
+                },
+                row: function () {
+                    $table.find("tbody").append(to_row(slice.call(arguments))); return this;
+                    return this;
+                },
+                caption: function (caption) {
+                    $table.find("caption").html(caption || "Caption"); return this;
+                },
+                err: function () {
+                    $table.find("tbody").append(to_row(
+                    "<span style='color:#cc0000;'>{0}</span>".format(slice.call(arguments).join(" ")))
+                    );
+                    return this;
+                },
+                uid: function (cb) { dbj.assert("function" == typeof cb);  cb(id); return this ; }
+            }
+        }
+        /*-----------------------------------------------------------------------------------------------*/
+    }; /* dbj.utl */
+    /*-----------------------------------------------------------------------------------------------*/
+};
