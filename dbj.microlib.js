ba3a9c54272a3e3d59529ae456c2207a9c052c5f
dbjdbj <dbjdbj@gmail.com>
2016-03-19 19:29:22 +0100

    few hours more and we are getting somewhere
    through the maelstrom of obvious bugs

--- dbj.microlib.js
+++ dbj.microlib.js
@@ -2,7 +2,7 @@
 /**
     DBJ*MICROLIB GPL (c) 2011 - 2016 by DBJ.ORG
 */
-;(function ( GLOBAL, undefined ) {
+(function ( GLOBAL, undefined ) {
 
 if ("object" == typeof dbj) {
 	var msg = "dbj core object must be defined only once and only in here";
@@ -10,6 +10,38 @@ if ("object" == typeof dbj) {
 	if ("function" == typeof alert) alert(msg);
 	return;
 };
+	/**
+	GLOBAL section -- is where we dare to create things in global scope
+	*/
+	/** (c) 2016 by DBJ.ORG
+	printf(a,b,c ...) ; printf(a,b,c ...)(d,e,f ... ).flush() ; printf(a,b,c ...).flush() ;
+	printf.element is where output goes to
+	printf.flush () empties the stored string to the innerText of element given or body if nothing is given as printf.element
+	printf.target = function (s) {} is used as (yes) printing target 
+
+	NOTE! be very carefull NOT to create global functions which do exist on window, in case of running in the presence of dom!
+	      for example naming this print will produce all sorts of weird bugs because window.print is defined in dom
+	*/
+		if ("function" == typeof printf) {
+			return alert("dbj MICROLIB want to create global printf()\n\nPlease rename it or simply remove it from inside dbj.microlib.js.");
+		}
+	(function () {
+		var print_buffer_ = (new Date()).toLocaleString() + "\n";
+
+		printf = function (s) {
+			var ags = [].slice.call(arguments);
+			printf.target(ags.join("\n"));
+			return printf;
+		}
+		var print_buffer_flush = function () { printf.element.innerText += print_buffer_; print_buffer_ = ""; return printf; }
+		printf.flush = print_buffer_flush;
+		printf.target = function (s_) {
+			print_buffer_ += ("\n" + s_);
+			if (print_buffer_.length > 0xFFFF) print_buffer_flush();
+		}
+		printf.element = document.body;
+	}());
+
      
 if ("function" !== typeof String.format) {
 /**
@@ -38,7 +70,7 @@ String.prototype.format = function () {
 var TOS_ = Object.prototype.toString, HOP_ = Object.prototype.hasOwnProperty; /*
  dbj core object must be defined only once and in here
  */
-var dbj = {
+GLOBAL.dbj = {
 
     "GLOBAL" : (function () {
 
@@ -57,18 +89,13 @@ var dbj = {
         }
     }()),
 	
-	keep_inside : (function () {
-		
-			var boundaries = [0, 1]; // default
-			
-			return function (val, bounds ) {
-				if (bounds) boundaries = bounds.sort();
+	keep_inside : function (val, bounds ) {
+				var boundaries = bounds ? bounds.sort() : [0,1];
 					val = val - 0;
 						if (isNaN(val)) throw new Error(0xFF,"dbj.keep_inside() received NaN value.");
 				return val > boundaries[1] ? boundaries[1] : (val < boundaries[0] ? boundaries[0] : val);
-			}
-	    }()), 
-	
+	},
+
 	err2str : function (e) {
 
     if (roleof(e) != "Error") return ""+e;
@@ -90,7 +117,7 @@ var dbj = {
 	        function () { return new ActiveXObject("Microsoft.XMLHTTP") }
         ];
 
-        dbj.createXHR = function () {
+        this.createXHR = function () {
             var i = XMLHttpFactories.length, e;
             while (i--) {
                 try {
@@ -103,7 +130,7 @@ var dbj = {
             throw ("dbj.createXMLHTTPObject() failed ?");
         }
 
-        return dbj.createXHR();
+        return this.createXHR();
     },
     $: function (selector, el) {
         return (el || document).querySelector(selector);
@@ -140,35 +167,31 @@ var dbj = {
 
     assert: function (x) {
         if (!(x)) {
-			
-            dbj.later(dbj, function () {
-                
-                var screamer = console ? console.error : alert ;
-                screamer("dbj.assert failed for: " + x + ", msg: " + x.message);
-            });
             throw new Error(0xFF,"dbj.assert(" + x + "), rendered to false");
         }
     },
 
     print: function () {
-		var ags = [].slice.call(arguments) ;
-        dbj.later(dbj, function () {
-            this.console.group();
-				for ( var j = 0; j < ags.length; j++ ) console.log(ags[j]);
-            this.console.group_end();
-        });
+    	if (console) {
+    		var ags = [].slice.call(arguments);
+    		this.console.group();
+    		for (var j = 0; j < ags.length; j++) console.log(ags[j]);
+    		this.console.group_end();
+    	} else
+			throw new Error(0xFF,"dbj.print() -- console does not exist?")
     },
 
-    evil: (function (x) {
+    evil: function (x) {
         // eval in presence of 'use strict'	
         "use strict";
         // spn = secret property name
         var spn = "evil_" + (+new Date());
-        return function (x) {
-            (1, eval)("top['" + spn + "'] = (" + x + ")");
-            return top[spn];
-        }
-    }()),
+        this.evil = function (x) {
+        	(1, eval)("window['" + spn + "'] = (" + x + ")");
+        	return top[spn];
+        };
+        return this.evil(x);
+    },
     /**
       numerical section follows
     */
@@ -188,12 +211,24 @@ var dbj = {
         return a - 0;
     },
     /* quick number rounder */
-    round: function (original_number, decimals) {
-        var POW = Math.pow(10, decimals),
-            V1 = original_number * POW,
-            V2 = Math.round(V1);
-        return V2 / POW;
+    round: function (f, places) {
+    	return +f.toFixed(places || 2);
     },
+
+	/** Pretty print an array. Examples:
+		a2s( create2Dset(2))     --> [[0,0][1,0]]
+		a2s( create2Dset(2),"!") --> [[0,0!1,0]]
+    */
+    a2s : function ( a, mid ) { 
+    	mid = mid || "][" ;
+    	return "[" + a.join(mid) + "]" ; 
+    },
+	/**	http://stackoverflow.com/questions/4959975/generate-random-value-between-two-numbers-in-javascript
+	*/
+    random : function (min,max)
+    {
+    	return Math.floor(Math.random()*(max-min+1)+min);
+    } ,
     /* http://www.2ality.com/2012/02/js-integers.html */
     toUint32: function (x) {
         return x >>> 0;
@@ -207,28 +242,31 @@ var dbj = {
      * returns unique values even if called in smallest possible intervals
      * eg: [seed(), seed(), seed()]
      */
-    seed: function () { return (Math.random() * (new Date() - 0)) >>> 0; }
+    seed: function () { return (Math.random() * (new Date() - 0)) >>> 0; },
+
+	/** Now this is positively evil, and irresistible ;) */
+    sum : function (arr) {
+    	this.assert( arr.length < 0xFF ); // at least some sanity check
+    	return dbj.evil(arr.join("+"));
+    },
+	avg : function (arr) {
+		this.assert(arr.length < 0xFF); // at least some sanity check
+		return this.sum(arr) / arr.length;
+		},
+
+	 GUID : function () {
+	    var empty = "00000000-0000-0000-0000-000000000000",
+            four = function () {
+            	return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1).toUpperCase();
+            };
+
+	    	this.GUID  = function () {
+	    		return (four() + four() + "-" + four() + "-" + four() + "-" + four() + "-" + four() + four() + four());
+	    	}
+		    	return this.GUID();
+        }
 
 }; /* eof dbj {} */
-
-        var Guid = {
-            empty: "00000000-0000-0000-0000-000000000000",
-            make: function () {
-                return (Guid.four() +
-                        Guid.four() + "-" +
-                        Guid.four() + "-" +
-                        Guid.four() + "-" +
-                        Guid.four() + "-" +
-                        Guid.four() +
-                        Guid.four() +
-                        Guid.four());
-            },
-            four: function () {
-                return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1).toUpperCase();
-            }
-        };
-
-        dbj.GUID = function () { return Guid.make(); }
 
 /* the stuff only old magician can produce :) */
 /** 
@@ -252,4 +290,4 @@ dbj.GUID = function ()
 }
 @end @*/
 
-}( this, (void 0)));
+}( this || window ));
